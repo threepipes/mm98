cd485d1bc19ccce8cda6eca57fff83afc8ed9ad9
Shogo Tsutsumi <s-tutumi@ist.osaka-u.ac.jp>
2018-02-16 18:04:29 +0900

    [add]真ん中速度低下 集合待ち 討伐 機能を追加

--- PrincessesAndMonsters/src/PrincessesAndMonsters.java
+++ PrincessesAndMonsters/src/PrincessesAndMonsters.java
@@ -48,6 +48,9 @@ public class PrincessesAndMonsters {
     }
     // ---CUT END---
 
+    final int[] diay = {1, 1, -1, -1};
+    final int[] diax = {1, -1, -1, 1};
+
     SecureRandom rand;
     String dir = "NEWS";
     char[] c;
@@ -99,6 +102,7 @@ public class PrincessesAndMonsters {
         cx = (int)(centerX / P);
 
         int widMax = Math.max(pBottom - pTop, pRight - pLeft);
+        widMax = Math.max(widMax, 3);
         widMax = Math.max(widMax, K / 3) / 2;
 //        widMax = K / 2;
         pLeft = Math.max(cx - widMax, 1);
@@ -111,8 +115,8 @@ public class PrincessesAndMonsters {
 //        pBottom = Math.min(pBottom + inc, S - 1);
         pCornerY = new int[]{pTop, pTop, pBottom, pBottom};
         pCornerX = new int[]{pLeft, pRight, pRight, pLeft};
-        cornerY = new int[]{0, 0, S, S};
-        cornerX = new int[]{0, S, S, 0};
+        cornerY = new int[]{0, 0, S - 1, S - 1};
+        cornerX = new int[]{0, S - 1, S - 1, 0};
 
         initC = -1;
         int near = S * S;
@@ -131,6 +135,11 @@ public class PrincessesAndMonsters {
         knight = new Knight[K];
         final int baseY = pCornerY[(initC + 1) % 4];
         final int baseX = pCornerX[(initC + 1) % 4];
+        int group = K / 10;
+        Loop[] loops = new Loop[group];
+        for (int i = 0; i < group; i++) {
+            loops[i] = new Loop(rand.nextBoolean() ? 1 : -1, rand.nextInt(S / 3) + 1, (initC + 2) % 4);
+        }
         for (int i = 0; i < K; i++) {
             final double vy = - baseY + pCornerY[(initC + 3) % 4];
             final double vx = - baseX + pCornerX[(initC + 3) % 4];
@@ -147,8 +156,9 @@ public class PrincessesAndMonsters {
                     new Pos(cornerY[(initC + 2) % 4], cornerX[(initC + 2) % 4]),
             });
             knight[i].setSpeedQueue(new double[]{
-                    1, 0.5, 1, 1,
+                    1, 1 - (1 - Math.pow((t - 0.5) * 2, 2)) * 0.2 - 0.2, 1, 1,
             });
+            knight[i].setLoop(loops[i % group].copy());
             knight[i].update();
         }
         ay = 0;
@@ -158,33 +168,87 @@ public class PrincessesAndMonsters {
         return new String(c);
     }
     int ax, ay;
+    int state = 0;
     public String move(int[] status, int P, int M, int timeLeft) {
         t++;
+        boolean stopAll = true;
         for (int i = 0; i < K; i++) {
-            c[i] = knight[i].move();
-            if(knight[i].goal()) {
-                knight[i].update();
-            }
+            if(status[i] < 0) continue;
+            Knight kn = knight[i];
+            if(state == 0 && kn.updateCount == 3 && kn.distNext() < 2) kn.stop();
+            if(state == 1 && kn.stop) kn.start();
+            if(state == 2 && kn.updateCount == 4 && kn.distNext() == 1 && status[i] == 0)
+                kn.update();
+            final int nc = getNearCorner(kn);
+            if(nc >= 0) kn.setTarget(cornerY[nc], cornerX[nc]);
+            else if(M == 0 && P == 0) kn.setTarget(cornerY[- nc - 1], cornerX[- nc - 1]);
+            c[i] = kn.move();
+            if(kn.goal()) kn.update();
+            if(!kn.stop) stopAll = false;
         }
+        if(stopAll) state++;
+        else if(state == 1) state++;
         return new String(c);
     }
+
+    int getNearCorner(Knight kn) {
+        int nearId = -1;
+        int nearDist = S * S;
+        for (int i = 0; i < 4; i++) {
+            final int d = kn.dist(cornerY[i], cornerX[i]);
+            if(d < nearDist) {
+                nearDist = d;
+                nearId = i;
+            }
+        }
+        final int tl = turnLeft();
+        return tl - 2 <= nearDist ? nearId : -nearId - 1;
+    }
+
+    int turnLeft() {
+        return S * S * S - t;
+    }
+
     int dist(int y1, int x1, int y2, int x2) {
         return Math.abs(y1 - y2) + Math.abs(x1 - x2);
     }
     double sigmoid(double a, double x) {
         return 1 / (1 + Math.pow(Math.E, -a * x));
     }
+
+
+    class Loop {
+        int cycle, inner, position;
+        Loop(int cycle, int inner, int position) {
+            this.cycle = cycle;
+            this.inner = inner;
+            this.position = position;
+        }
+
+        Pos next() {
+            final int y = cornerY[position] + diay[position];
+            final int x = cornerX[position] + diax[position];
+            position = (position + cycle + 4) % 4;
+            return new Pos(y, x);
+        }
+
+        Loop copy() {
+            return new Loop(cycle, inner, position);
+        }
+    }
 }
 
 class Knight {
     static SecureRandom rand;
     int i;
-    Pos p, t;
+    Pos p, t, d;
     Queue<Pos> targets;
     Queue<Double> speedQueue;
+    PrincessesAndMonsters.Loop loop;
     int updateCount = 0;
     double speed = 1;
     boolean stop = false;
+    int err;
 
     Knight(int i, int y, int x) {
         p = new Pos(y, x);
@@ -201,7 +265,11 @@ class Knight {
     }
 
     void setTarget(int y, int x) {
-        t.set(y, x);
+        t = new Pos(y, x);
+    }
+
+    void setLoop(PrincessesAndMonsters.Loop loop) {
+        this.loop = loop;
     }
 
     void setSpeedQueue(double[] ss) {
@@ -224,16 +292,24 @@ class Knight {
     }
 
     void update() {
-        if(targets.isEmpty()) return;
-        t = targets.poll();
-        speed = speedQueue.poll();
+        if(targets.isEmpty()) t = loop.next();
+        else {
+            t = targets.poll();
+            speed = speedQueue.poll();
+        }
         updateCount++;
+        d = new Pos(Math.abs(p.y - t.y), Math.abs(p.x - t.x));
+        err = d.x - d.y;
     }
 
     char move(int ty, int tx) {
         final int my = ty - p.y;
         final int mx = tx - p.x;
+        final int e2 = err * 2;
+//        if(e2 < d.x) {
+//        if((Math.abs(my) > Math.abs(mx) && p.dist(t) < 4) || e2 > -d.y) {
         if(Math.abs(my) > Math.abs(mx)) {
+            err += d.x;
             if(my < 0) {
                 p.y--;
                 return 'N';
@@ -242,6 +318,7 @@ class Knight {
                 return 'S';
             }
         } else {
+            err -= d.y;
             if(mx < 0) {
                 p.x--;
                 return 'W';
@@ -250,6 +327,14 @@ class Knight {
                 return 'E';
             } else return 'T';
         }
+    }
+
+    int dist(int y, int x) {
+        return p.dist(y, x);
+    }
+
+    int distNext() {
+        return p.dist(t);
     }
 }
 
@@ -262,6 +347,14 @@ class Pos {
     void set(int y, int x) {
         this.y = y;
         this.x = x;
+    }
+
+    int dist(int y, int x) {
+        return Math.abs(this.y - y) + Math.abs(this.x - x);
+    }
+
+    int dist(Pos p) {
+        return Math.abs(p.y - y) + Math.abs(p.x - x);
     }
 
     boolean eq(Pos p) {
