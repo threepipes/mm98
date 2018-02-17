dd80e2a8457c8814993462bf78e12a68142cbf7b
threepipes_s <threepipesatos@gmail.com>
2018-02-18 02:54:33 +0900

    [fix]Stateをenumに 囲いをやや拡大

--- PrincessesAndMonsters/src/PrincessesAndMonsters.java
+++ PrincessesAndMonsters/src/PrincessesAndMonsters.java
@@ -1,58 +1,12 @@
-import java.io.*;
 import java.security.*;
 import java.util.*;
 
 public class PrincessesAndMonsters {
 
-    // ---CUT START---
-    public static void main(String[] args) {
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-            PrincessesAndMonsters pam = new PrincessesAndMonsters();
-
-            int S = Integer.parseInt(br.readLine());
-            int P = Integer.parseInt(br.readLine());
-            int[] princesses = new int[P];
-            for (int i = 0; i < P; ++i) {
-                princesses[i] = Integer.parseInt(br.readLine());
-            }
-            int M = Integer.parseInt(br.readLine());
-            int[] monsters = new int[M];
-            for (int i = 0; i < M; ++i) {
-                monsters[i] = Integer.parseInt(br.readLine());
-            }
-            int K = Integer.parseInt(br.readLine());
-
-            String retInit = pam.initialize(S, princesses, monsters, K);
-            System.out.println(retInit);
-            System.out.flush();
-
-            for (int st = 0; ; ++st) {
-                int nK = Integer.parseInt(br.readLine());
-                int[] status = new int[nK];
-                for (int i = 0; i < nK; ++i) {
-                    status[i] = Integer.parseInt(br.readLine());
-                }
-
-                int nP = Integer.parseInt(br.readLine());
-                int nM = Integer.parseInt(br.readLine());
-                int timeLeft = Integer.parseInt(br.readLine());
-
-                String ret = pam.move(status, nP, nM, timeLeft);
-                System.out.println(ret);
-                System.out.flush();
-            }
-        }
-        catch (Exception e) {}
-    }
-    // ---CUT END---
-
     final int[] diay = {1, 1, -1, -1};
     final int[] diax = {1, -1, -1, 1};
 
     SecureRandom rand;
-    String dir = "NEWS";
     char[] c;
     int t;
     int S, P, M, K;
@@ -65,14 +19,14 @@ public class PrincessesAndMonsters {
     int[] cornerY, cornerX;
     int initC;
 
-    final int INC = 2;
-    final int GROUP_SIZE = 4;
+    static double SIGMOID_A = 3;
+    static long RAND_SEED = 1234;
 
     public String initialize(int S, int[] princesses, int[] monsters, int K) {
         try {
             rand = SecureRandom.getInstance("SHA1PRNG");
         } catch (Exception e) { return ""; }
-        rand.setSeed(1234);
+        rand.setSeed(RAND_SEED);
         Knight.rand = rand;
         t = -1;
 
@@ -103,16 +57,11 @@ public class PrincessesAndMonsters {
 
         int widMax = Math.max(pBottom - pTop, pRight - pLeft);
         widMax = Math.max(widMax, 3);
-        widMax = (Math.max(widMax, K / 3) + widMax) / 4;
-//        widMax = K / 2;
+        widMax = (Math.max(widMax, K / 3) + widMax) / 4 + S / 20;
         pLeft = Math.max(cx - widMax, 1);
         pRight = Math.min(cx + widMax, S - 2);
         pTop = Math.max(cy - widMax, 1);
         pBottom = Math.min(cy + widMax, S - 2);
-//        pLeft = Math.max(pLeft - inc, 0);
-//        pRight = Math.min(pRight + inc, S - 1);
-//        pTop = Math.max(pTop - inc, 0);
-//        pBottom = Math.min(pBottom + inc, S - 1);
         pCornerY = new int[]{pTop, pTop, pBottom, pBottom};
         pCornerX = new int[]{pLeft, pRight, pRight, pLeft};
         cornerY = new int[]{0, 0, S - 1, S - 1};
@@ -136,7 +85,7 @@ public class PrincessesAndMonsters {
         final int diag = (initC + 2) % 4;
         final int baseY = pCornerY[(initC + 1) % 4];
         final int baseX = pCornerX[(initC + 1) % 4];
-        int group = K / 10;
+        int group = 1;
         Loop[] loops = new Loop[group];
         for (int i = 0; i < group; i++) {
             loops[i] = new Loop(rand.nextBoolean() ? 1 : -1, rand.nextInt(S / 3) + 1, (initC + 2) % 4);
@@ -144,23 +93,59 @@ public class PrincessesAndMonsters {
         for (int i = 0; i < K; i++) {
             final double vy = - baseY + pCornerY[(initC + 3) % 4];
             final double vx = - baseX + pCornerX[(initC + 3) % 4];
-            final double t = sigmoid(3, rand.nextDouble() * 2 - 1);
+            final double t = sigmoid(SIGMOID_A, rand.nextDouble() * 2 - 1);
             Pos tPos = new Pos(
                     (int)(baseY + vy * t),
                     (int)(baseX + vx * t)
             );
             knight[i] = new Knight(i, cornerY[initC], cornerX[initC]);
-            knight[i].setTargetQueue(new Pos[]{
-                    new Pos(pCornerY[initC], pCornerX[initC]),
-                    tPos,
-                    new Pos(pCornerY[diag], pCornerX[diag]),
-                    new Pos(cornerY[diag], cornerX[diag]),
-            });
-            knight[i].setSpeedQueue(new double[]{
-                    1,
-                    1 - (1 - Math.pow((t - 0.5) * 2, 2)) * 0.2 - 0.1 - rand.nextDouble() * 0.1,
-                    1, 1,
-            });
+            if(false) {
+                knight[i].setTargetQueue(new Pos[]{
+                        new Pos(pCornerY[initC], pCornerX[initC]),
+                        tPos,
+                        new Pos(pCornerY[diag], pCornerX[diag]),
+                        tPos,
+                        new Pos(pCornerY[initC], pCornerX[initC]),
+                        new Pos(cornerY[initC], cornerX[initC]),
+                });
+                knight[i].setSpeedQueue(new double[]{
+                        1,
+                        // 必要なパラメータ: widMax, near
+                        1 - (1 - Math.pow((t - 0.5) * 2, 2)) * 0.2 - 0.1 - rand.nextDouble() * 0.1,
+                        1,
+                        1,// - (1 - Math.pow((t - 0.5) * 2, 2)) * 0.2 - 0.1 - rand.nextDouble() * 0.1,
+                        1,
+                        1,
+                });
+                knight[i].setStateQueue(new Count[]{
+                        Count.FirstCorner,
+                        Count.FirstDiagonal,
+                        Count.SecondCorner,
+                        Count.SecondDiagonal,
+                        Count.LastConer,
+                        Count.Finish,
+                });
+            } else {
+                knight[i].setTargetQueue(new Pos[]{
+                        new Pos(pCornerY[initC], pCornerX[initC]),
+                        tPos,
+                        new Pos(pCornerY[diag], pCornerX[diag]),
+                        new Pos(cornerY[diag], cornerX[diag]),
+                });
+                knight[i].setSpeedQueue(new double[]{
+                        1,
+                        // 必要なパラメータ: widMax, near
+                        1 - (1 - Math.pow((t - 0.5) * 2, 2)) * 0.2 - 0.1 - rand.nextDouble() * 0.1,
+                        1,
+                        1,
+                });
+                knight[i].setStateQueue(new Count[]{
+                        Count.FirstCorner,
+                        Count.FirstDiagonal,
+                        Count.LastConer,
+                        Count.Finish,
+                });
+            }
             knight[i].setLoop(loops[i % group].copy());
             knight[i].update();
         }
@@ -170,6 +155,7 @@ public class PrincessesAndMonsters {
         Arrays.fill(c, (char)('0' + initC));
         return new String(c);
     }
+
     int ax, ay;
     int state = 0;
     public String move(int[] status, int P, int M, int timeLeft) {
@@ -178,9 +164,10 @@ public class PrincessesAndMonsters {
         for (int i = 0; i < K; i++) {
             if(status[i] < 0) continue;
             Knight kn = knight[i];
-            if(state == 0 && kn.updateCount == 3 && kn.distNext() < 2) kn.stop();
+//            if(status[i] > 0 && kn.speed == 1) kn.speed = 0.9;
+            if(state == 0 && kn.updateCount == Count.LastConer && kn.distNext() < 2) kn.stop();
             if(state == 1 && kn.stop) kn.start();
-            if(state == 2 && kn.updateCount == 4 && kn.distNext() == 1 && status[i] == 0)
+            if(state == 2 && kn.updateCount == Count.Finish && kn.distNext() == 1 && status[i] == 0)
                 kn.update();
             final int nc = getNearCorner(kn);
             if(nc >= 0) kn.setTarget(cornerY[nc], cornerX[nc]);
@@ -241,14 +228,25 @@ public class PrincessesAndMonsters {
     }
 }
 
+enum Count {
+    Init,
+    FirstCorner,
+    FirstDiagonal,
+    SecondCorner,
+    SecondDiagonal,
+    LastConer,
+    Finish,
+}
+
 class Knight {
     static SecureRandom rand;
     int i;
     Pos p, t, d;
     Queue<Pos> targets;
     Queue<Double> speedQueue;
+    Queue<Count> stateQueue;
     PrincessesAndMonsters.Loop loop;
-    int updateCount = 0;
+    Count updateCount = Count.Init;
     double speed = 1;
     boolean stop = false;
     int err;
@@ -285,6 +283,11 @@ class Knight {
         targets.addAll(Arrays.asList(ts));
     }
 
+    void setStateQueue(Count[] cnt) {
+        stateQueue = new ArrayDeque<>();
+        stateQueue.addAll(Arrays.asList(cnt));
+    }
+
     char move() {
         if(stop || rand.nextDouble() > speed) return 'T';
         return move(t.y, t.x);
@@ -299,8 +302,8 @@ class Knight {
         else {
             t = targets.poll();
             speed = speedQueue.poll();
+            updateCount = stateQueue.poll();
         }
-        updateCount++;
         d = new Pos(Math.abs(p.y - t.y), Math.abs(p.x - t.x));
         err = d.x - d.y;
     }
